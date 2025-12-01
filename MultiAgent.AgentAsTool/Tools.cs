856a2c4a63543f10ef4dd7f85452bb507bab99dc
Ravi R <ravinderraturi@hotmail.com>
2025-12-01 21:30:09 +0530

    - Added sudo tools

---
+++ MultiAgent.AgentAsTool/Tools.cs
@@ -0,0 +1,16 @@
+﻿namespace MultiAgent.AgentAsTool;
+
+internal static class StringTools
+{
+    public static string UpperCase(string str) => str.ToUpper();
+    public static string LowerCase(string str) => str.ToLower();
+    public static string Reverse(string str) => new string(str.ToCharArray().Reverse().ToArray());
+}
+
+
+
+internal static class NumberTools
+{
+    public static int RandomNumber() => Random.Shared.Next();
+    public static int AnswerToEveryProblem() => 99;
+}
