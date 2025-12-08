8e55fbeca4cf474f1f671cacdad4de4fbb59ee3a
Ravi R <ravinderraturi@hotmail.com>
2025-12-08 17:57:07 +0530

    Added solution for CS Inc

---
+++ Demo/CS.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo;
+//For CS Inc.
+public static class StringHelper
+{
+
+
+    public static IEnumerable<string> ReturnSubString(string input)
+    {
+        IEnumerable<string> result = new List<string>();
+
+
+
+        for (int str = 0; str < input.Length; str++)
+        {
+
+            for (int final = str; final < input.Length; final++)
+            {
+                yield return input.Substring(str, final - str + 1);
+            }
+
+
+        }
+
+    }
+
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿//var substring = StringHelper.ReturnSubString("abcdef");
+
+int a = 5;
+int b = 6;
+
+
+
+a = a + b;//11
+
+
+
+b = a - b;//5
+Console.WriteLine(b);
+a = a - b;//6
+Console.WriteLine(a);
+
+
+Console.ReadLine();
+
+
