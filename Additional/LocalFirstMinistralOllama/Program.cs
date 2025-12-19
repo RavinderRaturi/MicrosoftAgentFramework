5c357d3d5a5c5c514ed8c645511bd8b2a2a3e762
Ravi R <ravinderraturi@hotmail.com>
2025-12-19 20:38:12 +0530

    init

---
+++ Additional/LocalFirstMinistralOllama/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+
+
+
+IChatClient chatClient =
+    new OllamaApiClient(new Uri("http://localhost:11434"), "ministral-3");
+
+var ollamaAgent = chatClient
+    .AsBuilder()
+    .UseFunctionInvocation()
+    .Build()
+    .CreateAIAgent(
+    name: "Ollama Local-First Agent",
+    instructions: "You are a helpful assistant that provides weather information."
+     );
+
